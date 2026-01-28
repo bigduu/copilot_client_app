48dd67b941761b96da70ce127e2552a5b9fdef51
bigduu <mugeng.du@gmail.com>
2026-01-29 01:16:10 +0800

    Fix Settings agent tab imports

---
+++ src/pages/SettingsPage/components/SystemSettingsPage/SystemSettingsAgentTab.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Card, Flex, Space, Typography, theme } from "antd";
+import { ClaudeInstallPanel } from "../../../AgentPage/components/ClaudeInstallPanel";
+import { ClaudeEnvironmentPanel } from "../../../AgentPage/components/ClaudeEnvironmentPanel";
+
+const { Text } = Typography;
+const { useToken } = theme;
+
+const SystemSettingsAgentTab: React.FC = () => {
+  const { token } = useToken();
+  const tabGap = token.marginLG;
+
+  return (
+    <Flex vertical gap={tabGap}>
+      <Card size="small">
+        <Space
+          direction="vertical"
+          size={token.marginXS}
+          style={{ width: "100%" }}
+        >
+          <Text strong>Claude Installer</Text>
+          <ClaudeInstallPanel />
+        </Space>
+      </Card>
+      <ClaudeEnvironmentPanel />
+    </Flex>
+  );
+};
+
+export default SystemSettingsAgentTab;
